d70bdfe3344aacfb9192256cc44ba9fd12bac2d8
sreyasag <sreyasag@gmail.com>
2022-10-10 15:33:37 +0530

    search input

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Header() {
   return (
     <div className='header'>
         <div className="header__left">
+            {/* <LinkedInIcon color='primary' fontSize='large'/> */}
+            <img 
+                src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" 
+                alt="" 
+            />
+
+            <div className="header__search">
+                <SearchIcon/>
+                <input type="text" />
+            </div>
 
         </div>
+
         <div className="header__right">
             
         </div>
+        
     </div>
   )
 }
